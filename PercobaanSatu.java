3e00b7cb52310ddd20ae1a2f6ecc90daaf56a3dc
rstubryan <96672455+rstubryan@users.noreply.github.com>
2022-10-25 08:54:28 +0700

    Update PercobaanSatu.java

--- PercobaanSatu.java
+++ PercobaanSatu.java
@@ -8,25 +8,23 @@
  *
  * @author Restu
  */
-public class PercobaanSatu {
-    class Parent {
-        public int x = 5;
-    }
+class Parent {
+    public int x = 5;
+}
 
-    class Child extends Parent {
-        public int x = 10;
-        public void Info(int x) {
-            System.out.println("Nilai x sebagai parameter = " + x);
-            System.out.println ("Data member x di class Child = " + this.x);
-            System.out.println ("Data member x di class Parent = " + super .x);
-        }
+class Child extends Parent {
+    public int x = 10;
+    public void Info(int x) {
+        System.out.println("Nilai x sebagai parameter = " + x);
+        System.out.println ("Data member x di class Child = " + this.x);
+        System.out.println ("Data member x di class Parent = " + super .x);
     }
-    
-    public class NilaiX {
-        public static void main(String[] args) {
-            Child tes = new Child();
-            tes.Info(20);
-        }
+}
+
+public class PercobaanSatu {
+    public static void main(String[] args) {
+        Child tes = new Child();
+        tes.Info(20);
     }
 }
 
