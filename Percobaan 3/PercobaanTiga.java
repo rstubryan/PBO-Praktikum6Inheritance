2a8e951b48ae1fefbd361cdfd71329e41e039e13
rstubryan <96672455+rstubryan@users.noreply.github.com>
2022-10-25 10:18:18 +0700

    Create PercobaanTiga.java

---
+++ Percobaan 3/PercobaanTiga.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Restu
+ */
+public class PercobaanTiga {
+    //kosong
+}
+
+class Child extends PercobaanTiga {
+    int x;
+    public Child() {
+        int x = 5;
+    }
+}
