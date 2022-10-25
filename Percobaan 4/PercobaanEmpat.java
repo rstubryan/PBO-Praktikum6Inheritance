2f94b45033ed87475f2340b65576ae210b27443b
rstubryan <96672455+rstubryan@users.noreply.github.com>
2022-10-25 10:37:10 +0700

    Update PercobaanEmpat.java

--- Percobaan 4/PercobaanEmpat.java
+++ Percobaan 4/PercobaanEmpat.java
@@ -1,1 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author Restu
+ */
+public class PercobaanEmpat {
+    public static void main(String[] args) {
+        Manager Utama = new Manager("John",5000000,"Financial");
+        System.out.println("Name:"+ Utama.GetName());
+        System.out.println("Salary:"+ Utama.GetSalary());
+        System.out.println("Department:"+ Utama.GetDept());
+        Utama = new Manager("Michael","Accounting");
+        System.out.println("Name:"+ Utama.GetName());
+        System.out.println("Salary:"+ Utama.GetSalary());
+        System.out.println("Department:"+ Utama.GetDept());
+    }
+}
