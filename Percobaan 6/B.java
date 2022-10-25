9a20a49adb333361ae47644a48eb218bf00ed8c8
rstubryan <96672455+rstubryan@users.noreply.github.com>
2022-10-25 10:22:41 +0700

    Add files via upload

---
+++ Percobaan 6/B.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Restu
+ */
+class B extends A{
+    B(){
+        System.out.println("Konstruktor B dijalankan ");
+        var_a = "Var_a dari class B";
+        var_b = "Var_a dari class B";
+    }
+}
