53370aebd317b5a369168e0e535b822cec29839b
rstubryan <96672455+rstubryan@users.noreply.github.com>
2022-10-25 10:31:17 +0700

    Update PercobaanLima.java

--- Percobaan 5/PercobaanLima.java
+++ Percobaan 5/PercobaanLima.java
@@ -1,1 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author Restu
+ */
+public class PercobaanLima {
+    protected String getMood(){
+        return "moody";
+    }
+    public void speak(){
+        System.out.println("I am "+getMood());
+    }
+    void laugh() {}
+    void cry() {}
+}
